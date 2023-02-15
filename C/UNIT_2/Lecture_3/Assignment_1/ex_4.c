a80b284167e444d95eb0815fd422ebbdd5da1698
wesam <wsamhamamm2002@gmail.com>
2023-02-15 19:07:39 +0200

    c

---
+++ C/UNIT_2/Lecture_3/Assignment_1/ex_4.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                       EX3:write c program 
+                       created on:15/2/2023
+                       outhor:wesam abdallah
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    float a,b;
+    printf("Enter two numbers:");
+    scanf("%f %f",&a,&b);
+    printf("product:%f",a*b);
+
+    return 0;
+}
+
