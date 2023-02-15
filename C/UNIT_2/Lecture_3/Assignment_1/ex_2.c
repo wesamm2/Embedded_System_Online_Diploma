e3ea795a8a561adf50d96f51b65d47aee17fe81c
wesam <wsamhamamm2002@gmail.com>
2023-02-15 18:58:33 +0200

    c

---
+++ C/UNIT_2/Lecture_3/Assignment_1/ex_2.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                       EX2:write c program to print asentence
+                       created on:15/2/2023
+                       outhor:wesam abdallah
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("Enter a integer:");
+    scanf("%d",&a);
+    printf("You entered:%d",a);
+
+    return 0;
+}
