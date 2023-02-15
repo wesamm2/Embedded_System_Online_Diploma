f97ba3711e71c6d9ba005f2fd84cde47af63aafa
wesam <wsamhamamm2002@gmail.com>
2023-02-15 19:16:08 +0200

    c

---
+++ C/UNIT_2/Lecture_3/Assignment_1/ex_5.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                       EX5:write c program 
+                       created on:15/2/2023
+                       outhor:wesam abdallah
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    char a;
+    printf("Enter a character:");
+    scanf("%c",&a);
+    printf("ASCII value of %c:%d",a,a);
+
+    return 0;
+}
+
