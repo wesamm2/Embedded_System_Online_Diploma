b3939d7874d01f31b10a658da11c2e525f3b95ec
wesam <wsamhamamm2002@gmail.com>
2023-02-15 19:04:43 +0200

    c

---
+++ C/UNIT_2/Lecture_3/Assignment_1/ex_3.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                       EX3:write c program 
+                       created on:15/2/2023
+                       outhor:wesam abdallah
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    printf("Enter two integer:");
+    scanf("%d %d",&a,&b);
+    printf("sum:%d",a+b);
+
+    return 0;
+}
+
